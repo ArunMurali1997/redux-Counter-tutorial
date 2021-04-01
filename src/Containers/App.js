46d26acafab149089851ef2c1c83dad131b5a1c8
Arun Murali <arunmurali1997@gmail.com>
2021-04-01 21:48:57 +0530

    counter with increment by 1 and decrement by 2 and save the latest state

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import Cout from "../components/Conteer/CounerOutput";
 import Cbutton from "../components/CounterControl/Cbutton";
@@ -9,8 +9,8 @@ import DisplayResult from "../components/CounterDIsplay/CounterDisplay";
 // import counter from "../components/Conteer/CounerOutput";
 // import { connect } from "react-redux";
 const App = (props) => {
-  const [counterState, setCounterState] = useState({ counter: 0, result: [] });
-  console.log(counterState.result[counterState.result.length]);
+  // const [counterState, setCounterState] = useState({ counter: 0, result: [] });
+  // console.log(counterState.result[counterState.result.length]);
 
   // const incHandler = () =>
   //   setCounterState({ ...counterState, counter: counterState.counter + 1 });
@@ -18,35 +18,36 @@ const App = (props) => {
   // console.log({ ...str, counter: str.counter++ });
 
   //return setCounterState({ ...str, counter: str.counter++ });
-  const onSaveHandler = () => {
-    const str = { ...counterState };
-    //let string = str.result[str.result.length];
+  // const onSaveHandler = () => {
+  //   const str = { ...counterState };
+  //   //let string = str.result[str.result.length];
 
-    setCounterState({
-      ...str,
-      result: [...str.result, str.counter],
-    });
+  //   setCounterState({
+  //     ...str,
+  //     result: [...str.result, str.counter],
+  //   });
 
-    //console.log(counterState.result);
-  };
+  //console.log(counterState.result);
+  // };
 
   return (
     <div className="App">
       <Cout value={props.ctr} />
       <Cbutton value={"Increment"} click={props.onIncrementCounter} />
       <Cbutton value={"Decrement by 2"} click={props.onDecrementBy2} val />
-      <Cbutton value={"Save"} click={onSaveHandler} />
-      <DisplayResult value={counterState.result}></DisplayResult>
+      <Cbutton value={"Save"} click={props.onStoreResult} />
+      <DisplayResult value={props.result}></DisplayResult>
     </div>
   );
 };
 const mapStaeToProps = (state) => {
-  return { ctr: state.counter, result: [] };
+  return { ctr: state.counter, result: state.result };
 };
 const mapDispatchToProps = (dispatch) => {
   return {
     onIncrementCounter: () => dispatch({ type: "INC" }),
     onDecrementBy2: () => dispatch({ type: "DEC2", value: 2 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,12 @@
 /* eslint-disable default-case */
-const rootReducer = (state = { counter: 0 }, action) => {
+const rootReducer = (state = { counter: 0, result: [] }, action) => {
   switch (action.type) {
     case "INC":
       return { ...state, counter: state.counter + 1 };
     case "DEC2":
       return { ...state, counter: state.counter - action.value };
+    case "STORE_RESULT":
+      return { ...state, result: [...state.result, state.counter] };
   }
   return state;
 };
