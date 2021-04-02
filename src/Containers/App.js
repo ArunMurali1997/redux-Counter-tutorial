6f71bba354abe4ed365e6867f87703743ea18d06
Arun Murali <arunmurali1997@gmail.com>
2021-04-02 17:27:22 +0530

    conter program with non repeated save

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import Cout from "../components/Conteer/CounerOutput";
 import Cbutton from "../components/CounterControl/Cbutton";
 import DisplayResult from "../components/CounterDIsplay/CounterDisplay";
-//import _ from "lodash/array";
 
 // import counter from "../components/Conteer/CounerOutput";
 // import { connect } from "react-redux";
@@ -32,22 +31,28 @@ const App = (props) => {
 
   return (
     <div className="App">
-      <Cout value={props.ctr} />
+      <Cout value={props.counter} />
       <Cbutton value={"Increment"} click={props.onIncrementCounter} />
-      <Cbutton value={"Decrement by 2"} click={props.onDecrementBy2} val />
+      <Cbutton value={"Decrement by 2"} click={props.onDecrementBy2} />
+      <Cbutton value={"Add By 10"} click={props.onAddtBy10} />
+      <Cbutton value={"divide By 10"} click={props.onDivideBy10} />
       <Cbutton value={"Save"} click={props.onStoreResult} />
       <DisplayResult value={props.result}></DisplayResult>
     </div>
   );
 };
+
 const mapStaeToProps = (state) => {
-  return { ctr: state.counter, result: state.result };
+  const { counter, result } = state;
+  return { counter, result };
 };
 const mapDispatchToProps = (dispatch) => {
   return {
     onIncrementCounter: () => dispatch({ type: "INC" }),
     onDecrementBy2: () => dispatch({ type: "DEC2", value: 2 }),
     onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onAddtBy10: () => dispatch({ type: "ADD10", value: 10 }),
+    onDivideBy10: () => dispatch({ type: "DIV10", value: 10 }),
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,20 @@
-/* eslint-disable default-case */
+import _ from "lodash";
+
 const rootReducer = (state = { counter: 0, result: [] }, action) => {
   switch (action.type) {
     case "INC":
       return { ...state, counter: state.counter + 1 };
     case "DEC2":
       return { ...state, counter: state.counter - action.value };
+    case "ADD10":
+      return { ...state, counter: state.counter + action.value };
+    case "DIV10":
+      return { ...state, counter: state.counter / action.value };
     case "STORE_RESULT":
-      return { ...state, result: [...state.result, state.counter] };
+      let res = [...state.result, state.counter];
+      return { ...state, result: _.uniq(res) };
+    default:
+      return state;
   }
-  return state;
 };
 export default rootReducer;
